0f972d0abeedfc76c23c89a5a117170763beeeaf
Si Thu Phyo <sithuphyo2225@gmail.com>
2022-03-20 19:24:08 +0630

    Fixed error

--- api_server.py
+++ api_server.py
@@ -10,7 +10,7 @@ import os
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
+    if os.system("dvc pull -r s3remote") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
